205a6e920a54d10d99eac4bdb8fdb32b2cc577d5
r41ng3l <alvaro.dedios.1@gmail.com>
2021-08-06 21:11:24 +0200

    added triangle.java

---
+++ Triangle.java
@@ -0,0 +1,28 @@
+class Triangle {
+	double area;
+	int height;
+	int length;
+	public static void main(String[] args) {
+		...
+		...
+		while ( ... ) {
+			...
+			... .height = (x + 1) * 2;
+			... .length = x + 4;
+			...
+			System.out.print("triangle " + x + " , area");
+			System.out.println(" = " + ... .area);
+			...
+		}
+		...
+		x = 27;
+		Triangle t5 = ta[2];
+		System.out.print("y = " + y);
+		System.out.println(", t5 area = " + t5.area);
+	}
+	void setArea() {
+		... = (height * length) / 2;
+	}
+}
+
+//triangle 0, area = 4.0
