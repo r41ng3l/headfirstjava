32d58c056845ba80f1ca2d60b43fb9a8f7c81489
r41ng3l <alvaro.dedios.1@gmail.com>
2021-09-09 19:34:53 +0200

    updating

--- Primos.java
+++ Primos.java
@@ -1,14 +1,20 @@
+import java.util.*;
+
 class Primos {
 	public static void main(String[] args) {
 
 		//Create array
-		int i = 1;
-		while (i <= 1000000) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter start number");
+		long i = scanner.nextInt();
+		System.out.println("Enter end number");
+		long x = scanner.nextInt();
+		while (i <= x) {
 			if (isPrime(i)) {
-				int j = i;
+				long j = i;
 				System.out.print(i);
 				while(true) {
-					if ((Integer.toString(Primos.cypherSum(j))).length() == 1) {
+					if ((Long.toString(Primos.cypherSum(j))).length() == 1) {
 						System.out.println(" ----> " + Primos.cypherSum(j));
 						break;
 					} else {
@@ -24,7 +30,7 @@ class Primos {
 	}
 
 	//Método para saber si un número dado es primo
-	static boolean isPrime(int primeTest) {
+	static boolean isPrime(long primeTest) {
 		if (
 		(primeTest % 2 != 0 && 
 		primeTest % 3 != 0 &&
@@ -47,11 +53,11 @@ class Primos {
 	}
 
 	// Método para sumar las cifras de un número e.g: 53 -> 8
-	static int cypherSum (int prime) {
+	static long cypherSum (long prime) {
 		String primeStr = new String();
-		primeStr = Integer.toString(prime);
-		int primeSum = 0;                                                     	
-                int k = 0;
+		primeStr = Long.toString(prime);
+		long primeSum = 0;                                                     	
+                long k = 0;
 		while (k < primeStr.length()) {
 	               	primeSum = Character.getNumericValue(primeStr.charAt(k)) + primeSum;
 			k++;
